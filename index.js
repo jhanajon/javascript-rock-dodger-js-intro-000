c3e25ae4da7d0c7ec1af0047e5109b75dee479d1
joe fisher <jhanajon@gmail.com>
2018-08-10 03:15:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ function createRock(x) {
   rock.className = 'rock';
   rock.style.left = `${x}px`
   var top = rock.style.top = 0
-  rock.style.top = top
   GAME.appendChild(rock)
  
   function moveRock(top) {
