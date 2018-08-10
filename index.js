9248f49d513d93a1c2599bcb042e6236410ca19d
joe fisher <jhanajon@gmail.com>
2018-08-10 03:10:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function checkCollision(rock) {
     const rockLeftEdge = positionToInteger(rock.style.left)
     const rockRightEdge = rockLeftEdge + 20;
 
-    return 
+    return (
     (((rockLeftEdge <= dodgerLeftEdge ) && ( rockRightEdge >= dodgerLeftEdge )) ||
           (( rockLeftEdge > dodgerLeftEdge) && ( rockRightEdge <= dodgerRightEdge)) ||
               (( rockLeftEdge < dodgerRightEdge ) && ( rockRightEdge > dodgerRightEdge ))
