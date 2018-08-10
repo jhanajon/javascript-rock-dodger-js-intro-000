74a5ed71ba102f7b3b84dbddaf0092146a73b284
joe fisher <jhanajon@gmail.com>
2018-08-10 03:14:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function createRock(x) {
   const rock = document.createElement('div')
   rock.className = 'rock';
   rock.style.left = `${x}px`
-  var top = 0
+  var top = rock.style.top = 0
   rock.style.top = top
   GAME.appendChild(rock)
  
