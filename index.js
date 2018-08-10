9d7686592171135fe2e4790e5ae84a87e0ff8ed7
joe fisher <jhanajon@gmail.com>
2018-08-10 03:26:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function createRock(x) {
 function endGame() {
     clearInterval(gameInterval)
     
-   ROCKS.forEach(function(rock) { rock.remove() })
+    ROCKS.forEach(function(rock) { rock.remove() })
     }
     
     document.removeEventListener('keydown', moveDodger)
