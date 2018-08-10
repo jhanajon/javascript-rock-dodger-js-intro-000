7fe9ee959a9219ed93e152bf8b3d4bb7ba933a20
joe fisher <jhanajon@gmail.com>
2018-08-10 02:56:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function createRock(x) {
  
   function moveRock(top) {
       rock.style.top = `${top += 2}px`
-      if ( top > 360 ) {
+      if ( top < 360 ) {
         window.requestAnimationFrame(moveRock)
     }
   }
