1aa54e177d83de6f6c6536e17b067d65d7b2f900
joe fisher <jhanajon@gmail.com>
2018-08-10 03:35:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -118,16 +118,24 @@ function moveDodgerLeft() {
 //   }
 // }
    
-
-
-
 function moveDodgerRight() {
-        var rightNumbers = dodger.style.left.replace('px', '')
-        var right = parseInt(rightNumbers, 10)
-        if (right < 360 ) {
-    dodger.style.left = `${right + 4}px`
-   }
+  window.requestAnimationFrame(function() {
+    const left = positionToInteger(DODGER.style.left)
+
+    if (left < 360) {
+      DODGER.style.left = `${left + 4}px`;
+    }
+  })
 }
+
+
+// function moveDodgerRight() {
+//         var rightNumbers = dodger.style.left.replace('px', '')
+//         var right = parseInt(rightNumbers, 10)
+//         if (right < 360 ) {
+//     dodger.style.left = `${right + 4}px`
+//   }
+// }
 
 
 
