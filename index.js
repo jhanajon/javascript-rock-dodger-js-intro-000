a1006bdabab2b911fdcbfa5bfad73c8cbd159b2d
joe fisher <jhanajon@gmail.com>
2018-08-10 03:06:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -105,6 +105,6 @@ function start() {
   START.style.display = 'none'
 
   gameInterval = setInterval(function() {
-    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 2)))
+    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
   }, 1000)
 }
