fd796a81486ef34d7dc92499c6909f97aa185367
joe fisher <jhanajon@gmail.com>
2018-08-10 03:11:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ function checkCollision(rock) {
 
     return (
     (((rockLeftEdge <= dodgerLeftEdge ) && ( rockRightEdge >= dodgerLeftEdge )) ||
-          (( rockLeftEdge > dodgerLeftEdge) && ( rockRightEdge <= dodgerRightEdge)) ||
-              (( rockLeftEdge < dodgerRightEdge ) && ( rockRightEdge > dodgerRightEdge ))
-               ) {
+    (( rockLeftEdge > dodgerLeftEdge) && ( rockRightEdge <= dodgerRightEdge)) ||
+    (( rockLeftEdge < dodgerRightEdge ) && ( rockRightEdge > dodgerRightEdge ))
+      ) {
      
     }
   }
