09e12344ff4ce2a6d33396ac16d9ce7ea7efe03b
joe fisher <jhanajon@gmail.com>
2018-08-10 03:17:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ function createRock(x) {
       if (top < GAME_HEIGHT) {
         window.requestAnimationFrame(moveRock)
     }
+    else 
   }
   if ( checkCollision(rock) ) {
     return endGame()
